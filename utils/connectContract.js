13e38e278737453c2824cb9d5e790e47f003dc86
Annie <ahua7@gatech.edu>
2022-08-25 01:43:56 -0400

    frontend changes

---
+++ utils/connectContract.js
@@ -0,0 +1,26 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+    //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
+    const contractAddress = "0xcc1b17B9dc6487FbEf9E87E092ddDCFb66969aAc";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        //checking for eth object in the window
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+    return rsvpContract;
+  }
+  
+  export default connectContract;
